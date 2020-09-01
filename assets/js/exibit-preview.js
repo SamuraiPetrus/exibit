1ad2072e6c05c2a6a9f06b817adc59cbf53b1080
Petrus Nogueira <petrusyagami@gmail.com>
2020-09-01 13:25:42 -0300

    adicionando funcionalidade de upload, ainda sem salvar nada no banco de dados

---
+++ assets/js/exibit-preview.js
@@ -0,0 +1,20 @@
+
+//Script que lida com a prévia de imagem na edição.
+
+$('#exibit_preview').change(function (event) {
+
+  //Adicionando imagem de prévia
+  var image = this.files[0];
+  $("#imagePreviewContainer").innerHTML = '';
+  var imgElement = document.createElement("img");
+  imgElement.classList.add("preview_box_image");
+  imgElement.src = window.URL.createObjectURL(image);
+  imgElement.alt = image.name;
+  imgElement.onload = function () {
+      window.URL.revokeObjectURL(this.src);
+  };
+  document.getElementById("preview_box").innerHTML = ''; // clear existing content
+  $("#preview_box").append(imgElement);
+
+  //Acionando botão de Excluir imagem
+});
--- exibit.php
+++ exibit.php
@@ -14,6 +14,13 @@ add_action("init", "_exibit_main");
 
 function _exibit_main ( $exibit_methods ) {
 
+  //Adicionando ENCTYPE
+  add_action( 'post_edit_form_tag' , 'post_edit_form_tag' );
+
+  function post_edit_form_tag( ) {
+      echo ' enctype="multipart/form-data"';
+  }
+
   add_action( "add_meta_boxes", "exibit_metabox_add" );
   function exibit_metabox_add () {
     add_meta_box(
@@ -27,83 +34,17 @@ function _exibit_main ( $exibit_methods ) {
   }
 
   function exibit_html ( $the_post ) {
+    wp_nonce_field( 'exibit_metabox_nonce', 'exibit_nonce' );
     ?>
-    <style media="screen">
-
-      /* Geral */
-      .exibit{
-        display: flex;
-        flex-wrap: wrap;
-        margin: 20px 0 !important;
-      }
-      .exibit ul, .exibit figure {
-        margin: 0;
-      }
-
-      /* Painel de controle */
-      .settings{
-        width: 100%;
-        display: flex;
-        justify-content: flex-end;
-        margin-bottom: 20px;
-      }
-      .settings button{
-        margin-right: 10px !important;
-      }
-      .settings button:last-child{
-        margin-right: 0px !important;
-      }
-
-      /* Vetores */
-      .vetores{
-        padding-right: 25px !important;
-        margin-right: 25px !important;
-        width: 32%;
-        max-height: 530px;
-        overflow-y: scroll;
-      }
-      .vetor{
-        background: #f1f1f1;
-        border: 1px solid lightgray;
-        padding: 20px;
-        margin-bottom: 10px;
-      }
-      .vetor input{
-        width: 100%;
-        margin-bottom: 10px;
-      }
-      .dimensions{
-        display: flex;
-        justify-content: space-between;
-      }
-      .dimensions input{
-        width: 48%;
-      }
-
-      /* Prévia de exibição */
-      .previa{
-        width: 500px;
-        height: 468.58px;
-        border: 2px solid lightgray;
-        color: lightgray;
-        font-size: 50px;
-        position: relative;
-      }
-      .previa figcaption{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-      }
-
-    </style>
+    <link rel="stylesheet" href="<?=plugins_url("assets/css/exibit-preview.css", __FILE__)?>">
     <div class="exibit">
       <div class="settings">
-        <button type="button" class="button tagadd" name="button">Adicionar vetor</button>
-        <button type="button" class="button tagadd" name="button">Upload da imagem</button>
+        <button type="button" class="button tagadd" name="button" disabled>Adicionar vetor</button>
+        <button type="button" name="button" class="button tagadd"><input type="hidden" name="MAX_FILE_SIZE" value="30000" /><input id="exibit_preview" type="file" accept="image/*" class="upload_preview" name="exibit_preview" />Upload da imagem</button>
+        <button type="button" class="button tagadd exclude" name="button" disabled>Excluir imagem atual</button>
       </div>
       <ul class="vetores">
-        <li class="vetor">
+        <!-- <li class="vetor">
             <input type="text" placeholder="Nome" name="nome" value="" />
             <input type="text" placeholder="Fonte" name="fonte" value="" />
             <input type="text" placeholder="Tamanho" name="tamanho" value="" />
@@ -111,31 +52,35 @@ function _exibit_main ( $exibit_methods ) {
               <input type="text" placeholder="X" name="dimensions_x" value="" />
               <input type="text" placeholder="Y" name="dimensions_y" value="" />
             </div>
-        </li>
-        <li class="vetor">
-            <input type="text" placeholder="Nome" name="nome" value="" />
-            <input type="text" placeholder="Fonte" name="fonte" value="" />
-            <input type="text" placeholder="Tamanho" name="tamanho" value="" />
-            <div class="dimensions">
-              <input type="text" placeholder="X" name="dimensions_x" value="" />
-              <input type="text" placeholder="Y" name="dimensions_y" value="" />
-            </div>
-        </li>
-        <li class="vetor">
-            <input type="text" placeholder="Nome" name="nome" value="" />
-            <input type="text" placeholder="Fonte" name="fonte" value="" />
-            <input type="text" placeholder="Tamanho" name="tamanho" value="" />
-            <div class="dimensions">
-              <input type="text" placeholder="X" name="dimensions_x" value="" />
-              <input type="text" placeholder="Y" name="dimensions_y" value="" />
-            </div>
-        </li>
+        </li> -->
       </ul>
-      <figure class="previa">
+      <div id="preview_box" class="previa">
         <figcaption>624 x 585</figcaption>
-      </figure>
+      </div>
+      <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+      <script type="text/javascript" src="<?=plugins_url("assets/js/exibit-preview.js", __FILE__)?>"></script>
     </div>
     <?php
+  }
+
+  add_action( 'save_post', 'vinil_meta_box_save' );
+
+  function vinil_meta_box_save( $post_id )
+  {
+    if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+
+    if( !isset( $_POST['exibit_nonce'] ) || !wp_verify_nonce( $_POST['exibit_nonce'], 'exibit_metabox_nonce' ) ) return;
+
+    if( !current_user_can( 'edit_post' ) ) return;
+
+    $allowed = array(
+    'a' => array(
+    'href' => array()
+    )
+    );
+
+    if( isset( $_POST['exibit_preview'] ) )
+    update_post_meta( $post_id, 'texto_meta_box', wp_kses( $_POST['exibit_preview'], $allowed ) );
   }
 
 }
