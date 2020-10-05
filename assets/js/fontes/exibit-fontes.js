beb7f606667812b192543170fd1ac7ed87aaec27
Petrus Nogueira <petrusyagami@gmail.com>
2020-10-05 12:25:42 -0300

    fontes chamadas via ajax

--- assets/js/fontes/exibit-fontes.js
+++ assets/js/fontes/exibit-fontes.js
@@ -6,7 +6,7 @@
 
 var fontes_do_projeto = (function ( vetor_id ) {
     var httpRequest,
-    url = "../wp-json";
+    url = "../wp-json/wp/v2/exibit_fontes";
 
     if ( window.XMLHttpRequest ) {
         httpRequest = new XMLHttpRequest();
@@ -18,16 +18,24 @@ var fontes_do_projeto = (function ( vetor_id ) {
         if ( httpRequest.readyState === 4 ) {
             if ( httpRequest.status === 200 ) {
                 var painel = document.getElementById(vetor_id),
-                fontes_object = JSON.parse( httpRequest.responseText ),
-                fonte = document.createElement("option");
-                fonte.innerHTML = fontes_object.description;
-                fonte.value = "weart";
+                fontes_object = JSON.parse( httpRequest.responseText );
 
                 painel.childNodes.forEach(function( child ){
-                    // console.log( child );
                     if ( child.classList.contains( "exibit-vetor-fontes" ) ) {
                         child.innerHTML = "";
-                        child.append( fonte );
+                        //Laço de repetição mediante as fontes cadastradas no site
+                        fontes_object.forEach(function( fonte ){
+                            var id = fonte.id,
+                            title = fonte.title.rendered;
+
+                            if ( id && title ) {
+                                var option = document.createElement("option");
+                                option.value = id;
+                                option.innerHTML = title;
+
+                                child.append( option );
+                            }
+                        });
                     }
                 });
             }
